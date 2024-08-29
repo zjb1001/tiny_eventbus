7739972cc48f2e9d1d5422e641dd1dcefa27e8c8
zjb1001 <zjb1001@gmail.com>
2024-08-29 19:11:02 +0800

    Update command line example code

--- examples/subscriber_command.c
+++ examples/subscriber_command.c
@@ -12,26 +12,26 @@ int main(int argc, char *argv[]) {
     
     char bus[MAX_BUS_NAME];        strcpy(bus, argv[1]);
     char topic[MAX_TOPIC_NAME];    strcpy(topic, argv[2]);
-    int subscriber_id; subscriber_id = (argc == 4) ? atoi(argv[3]) : 0; 
+
 
     if (!eventbus_connect(bus) ) {
         fprintf(stderr, "Failed to connect to event bus '%s'\n", argv[1]);
         return 1;
     }
 
-    if (eventbus_subscribe_to_topic(topic, subscriber_id) != 0) {
+    if (eventbus_subscribe_to_topic(topic) != 0) {
         fprintf(stderr, "Failed to subscribe to topic 'hello'\n");
         eventbus_disconnect();
         return 1;
     }
 
-    printf("Subscriber %d connected to bus '%s' and subscribed to topic 'hello'\n", subscriber_id, argv[1]);
+    printf("Connected to bus '%s' and subscribed to topic 'hello'\n", argv[1]);
     printf("Listening for messages... (Press Ctrl+C to exit)\n");
 
     while (1) {
         char* message = eventbus_read_message(topic);
         if (message && strlen(message) > 0) {
-            printf("Subscriber %d received: %s\n", subscriber_id, message);
+            printf("Received: %s\n", message);
         }
         sleep(1);
     }
--- examples/subscriber_interative.c
+++ examples/subscriber_interative.c
@@ -5,29 +5,38 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s <bus_name> <subscriber_id>\n", argv[0]);
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <bus_name>\n", argv[0]);
         return 1;
     }
 
-    int subscriber_id = atoi(argv[2]);
+    int subscriber_id = getpid();
+    char bus[MAX_BUS_NAME];        strcpy(bus, argv[1]);
+    char topic[MAX_TOPIC_NAME];  
 
-    if (!eventbus_connect(argv[1])) {
-        fprintf(stderr, "Failed to connect to event bus '%s'\n", argv[1]);
+    if (!eventbus_connect(bus)) {
+        fprintf(stderr, "Failed to connect to event bus '%s'\n", bus);
         return 1;
     }
-
-    if (eventbus_subscribe_to_topic("hello", subscriber_id) != 0) {
-        fprintf(stderr, "Failed to subscribe to topic 'hello'\n");
-        eventbus_disconnect();
-        return 1;
-    }
-
-    printf("Subscriber %d connected to bus '%s' and subscribed to topic 'hello'\n", subscriber_id, argv[1]);
-    printf("Listening for messages... (Press Ctrl+C to exit)\n");
 
     while (1) {
-        char* message = eventbus_read_message("hello");
+        printf("Enter a topic (or 'quit' to exit): ");
+        fgets(topic, sizeof(topic), stdin);
+        topic[strcspn(topic, "\n")] = 0;  // Remove newline
+
+        if (strcmp(topic, "quit") == 0) {
+            break;
+        }
+
+        if (eventbus_subscribe_to_topic(topic) != 0) {
+            fprintf(stderr, "Failed to subscribe to topic %s\n", topic);
+            eventbus_disconnect();
+            return 1;
+        }
+
+        printf("Subscriber %d connected to bus '%s' and subscribed to topic %s\n", subscriber_id, bus, topic);
+
+        char* message = eventbus_read_message(topic);
         if (message && strlen(message) > 0) {
             printf("Subscriber %d received: %s\n", subscriber_id, message);
         }
